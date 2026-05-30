8aaa1d49ebadb37aed152beae4deee643e712aad
Coffee0xff_256 <mateo.rodriguez5@udea.edu.co>
2026-05-29 22:38:39 -0500

    modificacion y arreglos del menu principal del juego

--- Final_project/mainwindow.h
+++ Final_project/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QLabel>
+#include <QPixmap>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -24,7 +27,23 @@ private slots:
 
     void on_botton_exit_clicked();
 
+    //aqui viene el slot para lo del movimiento del background emma
+    void background_move();
+
+
+    void on_label_linkActivated(const QString &link);
+
 private:
     Ui::MainWindow *ui;
+    //atributos para lo del background emma del menu
+    QLabel *background;
+    QTimer *timer;
+    int position_x;
+    // end menu //
+    //mejoramiento del menu//
+    QLabel* background1;
+    QLabel* background2;
+
+
 };
 #endif // MAINWINDOW_H
