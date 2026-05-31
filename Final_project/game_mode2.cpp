cfb62a1a5aa34b03060aa8ed5146a241dc858f5e
Coffee0xff_256 <mateo.rodriguez5@udea.edu.co>
2026-05-31 13:04:42 -0500

    Etapa 3, movimiento y dirección de visíon, eso está muy tosco, no puedo rotar ni mirar a la vez

--- Final_project/game_mode2.cpp
+++ Final_project/game_mode2.cpp
@@ -7,6 +7,10 @@ Game_mode2::Game_mode2(QWidget *parent)
     resize(1080,720);
     setWindowTitle("Game_mode1");
 
+//***********************Movimiento********************************
+    setFocusPolicy(Qt::StrongFocus);
+//***********************Movimiento********************************
+
 }
 //************ESQUELETO*****************************
 void Game_mode2::paintEvent(QPaintEvent *event){
@@ -39,4 +43,59 @@ painter.drawEllipse(
     ;
 
 //***********Mapa vista cenital**********************
+
+//***********************Movimiento********************************
+    //Aqui hay movimiento y visión
+
+    int linelenght = 30;
+    int x1 = playerx * tilesize;
+    int y1 = playery * tilesize;
+    int x2 = x1 + cos(angle) * linelenght;
+    int y2 = y1 + sin(angle) * linelenght;
+    painter.setPen(QPen(Qt::green,2));
+    painter.drawLine(x1,y1,x2,y2);
+
+//***********************Movimiento********************************
 }
+
+//***********************Movimiento********************************
+
+void Game_mode2::keyPressEvent(QKeyEvent *event){
+    double velocidad = 0.2;
+    double rotacion = 0.1;
+
+    switch(event->key()){
+        playerx += cos(angle) * velocidad;
+        playery += sin(angle) * velocidad;
+
+        break;
+
+    case Qt::Key_W:
+
+        playerx += cos(angle) * velocidad;
+        playery += sin(angle) * velocidad;
+
+        break;
+
+    case Qt::Key_S:
+
+        playerx -= cos(angle) * velocidad;
+        playery -= sin(angle) * velocidad;
+
+        break;
+
+    case Qt::Key_A:
+
+        angle -= rotacion;
+
+        break;
+
+    case Qt::Key_D:
+
+        angle += rotacion;
+
+        break;
+    }
+    update();
+}
+//***********************Movimiento********************************
